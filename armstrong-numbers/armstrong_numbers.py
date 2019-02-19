ca4f95fb8d0fb84b5d40539981c3d38bf2ce12d5
Kenia <keniavalladarez@gmail.com>
2019-02-19 16:02:41 -0500

    Solution for armstrong numbers

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,9 @@
 def is_armstrong(number):
-    pass
+    numbers = [int(n) for n in str(number)]
+    length = len(numbers)
+
+    total = 0
+    for num in numbers:
+        total += num**length
+
+    return total == number
