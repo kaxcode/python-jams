6783dfb91e94dbbcff3f83d555326febf24f6668
Kenia <keniavalladarez@gmail.com>
2019-02-19 14:17:33 -0500

    Solution for robot name

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,18 @@
+import random
+import string
+
+
 class Robot(object):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name=None):
+        if name is None:
+            name = self.generate_name()
+
+        self.name = self.generate_name()
+
+    def reset(self):
+        self.name = self.generate_name()
+
+    def generate_name(self):
+        letters = ''.join(random.sample(string.ascii_uppercase, 2))
+        numbers = random.randrange(0, 999, 3)
+        return letters + str(numbers)
