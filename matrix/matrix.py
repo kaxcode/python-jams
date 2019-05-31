a70e92cd66338ed6ce20dc77b04281bd74fca6e0
Kenia <keniavalladarez@gmail.com>
2019-05-31 15:09:53 -0400

    Solution for matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,18 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix = matrix_string.split("\n")
+        print(self.matrix)
 
     def row(self, index):
         return[int(i) for i in self.matrix[index-1].split(" ")]
 
     def column(self, index):
-        return self.matrix
+        empty_list = []
+
+        for string in self.matrix:
+            list_of_lists = []
+            list_of_lists.append(string.split(" "))
+            for item in list_of_lists:
+                empty_list.append(item[index-1])
+
+        return[int(i) for i in empty_list]
