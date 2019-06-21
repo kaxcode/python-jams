c9e9bf2718a1c4ba04f1dd9300918c39d838cd12
Kenia <keniavalladarez@gmail.com>
2019-06-21 10:49:19 -0400

    Refactoring matrix

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,16 +1,22 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.matrix = int(matrix_string.split("\n"))
+        ms = matrix_string.split("\n")
+
+        list_of_lists = []
+        for x in ms:
+            list_of_lists.append([int(i) for i in x.split(" ")])
+
+        self.matrix = list_of_lists
 
     def row(self, index):
-        return self.matrix[index-1].split(" ")
+        return self.matrix[index-1]
 
     def column(self, index):
         empty_list = []
 
         for string in self.matrix:
             list_of_lists = []
-            list_of_lists.append(string.split(" "))
+            list_of_lists.append(string)
             for item in list_of_lists:
                 empty_list.append(item[index-1])
 
